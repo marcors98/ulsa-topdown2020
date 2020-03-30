a59ea23f92fd27af128e6751b32925f5eed5b999
Marco Antonio Rodríguez <marcosrodso@gmail.com>
2020-03-30 13:34:22 -0700

    cinemachine

---
+++ Assets/Scripts/Inventory/Potion.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Potion", menuName = "Items/Potion", order = 1)]
+public class Potion : ScriptableObject
+{
+    [SerializeField] string objectName = "Potion";
+    [SerializeField] int healthRestore;
+    [SerializeField, TextArea(3, 10)] string description;
+
+    public int HealthRestore { get => healthRestore; }
+}
